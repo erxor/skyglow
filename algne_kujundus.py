9bab637817f2662be99e07a684f619a20511da0d
MadisKarli <matukoppel@gmail.com>
2013-12-10 13:18:58 +0200

    Scrollbar töötab, näeb kordades koledam välja tho.

--- algne_kujundus.py
+++ algne_kujundus.py
@@ -7,6 +7,7 @@ from tkinter import ttk
 from twitter import *
 from os.path import isfile
 from tkinter import font
+from textwrap import * 
 
 def validateTextInputSize(event):#Loendur toodab mingit jama counteri järgi, kui see läheb alla 10, võiks kohe ilmuda
      arv = (140-(len(twiidikast.get('1.0',END))-1))
@@ -25,56 +26,68 @@ def validateTextInputSize(event):#Loendur toodab mingit jama counteri järgi, ku
      
 def säutsumine():
     if len(twiidikast.get('1.0',END))-1 >140:
-        messagebox.showinfo("Viga!", message = "Twiit on liialt pikk. Maksimaalne lubatud pikkus on 140 tähemärki")
+        #messagebox.showinfo("Viga!", message = "Twiit on liialt pikk. Maksimaalne lubatud pikkus on 140 tähemärki")
+        pass
     else:
-         twitter.statuses.update(status=twiidikast.get('1.0',END))
-         twiidikast.delete('1.0', END)
-         twiidikast.insert('1.0', 'Sisesta siia oma tweet...')
-         twiidikast.tag_add('hall tekst', '1.0', 'end')
+        twitter.statuses.update(status=twiidikast.get('1.0',END))
+        twiidikast.delete('1.0', END)
+        twiidikast.insert('1.0', 'Sisesta siia oma tweet...')
+        twiidikast.tag_add('hall tekst', '1.0', 'end')
      
-def kustuta_tekst(lambimuutuja):
+def kustuta_tekst(event):
     twiidikast.delete('1.0', END)
     twiidikast.configure(height = 5)
 
 def twiit(a,b,c):
-    twiit = ttk.Label(raam, wraplength = 310,text = a)
-    twiit.configure(background = color1, foreground = "white")
-    twiit.place(x = b, y = c)
+    #twiit = ttk.Label(raam, wraplength = 310,text = a)
+    #twiit.configure(background = color1, foreground = "white")
+    #twiit.place(x = b, y = c)
+    b = c
+
+def parem_twiit(name, user, tweet, yasukoht):
+        tweet = wrap(tweet, width = 68)
+        if len(tweet) == 1:
+             tweet = tweet[0]
+        elif len(tweet) == 2:
+             tweet = tweet[0]+'\n'+tweet[1]
+        elif len(tweet) == 3:
+             tweet = tweet[0]+'\n'+tweet[1]+'\n'+tweet[2]
+        twiidiala.create_text(0,yasukoht+5, text = (name+'   '+"@"+user+"\n"+tweet+'\n'), anchor = 'w', font =("Segoe UI", 8), fill = "white")
 
 def get_tweets_mina():
     c = 25
+    twiidiala.delete(ALL)
     statuses = twitter.statuses.user_timeline()
-    for a in range(0,5):
+    for a in range(0,12):
         x = statuses[a]
         tweet = x['text']
         user = (x['user'])['screen_name']
         name = (x['user'])['name']
-        b = 320
-        twiit(name+'   '+"@"+user+"\n"+tweet+'\n'*2, b, c)
+        parem_twiit(name, user, tweet, c)
         c += 70
 
 def get_tweets():
     c = 25
+    twiidiala.delete(ALL)
     statuses = twitter.statuses.home_timeline()
-    for a in range(0,5):
+    for a in range(0,11):
         x = statuses[a]
         tweet = x['text'] #krabame dictist teksti
         user = (x['user'])['screen_name'] #krabame dicti subdictist username
         name = (x['user'])['name'] #lisaks usernamele võtaks silmale meeldivama nime ka
-        b = 320
-        twiit(name+'   '+"@"+user+"\n"+tweet+'\n'*2, b, c) #prindime välja
+        parem_twiit(name, user, tweet, c)
         c += 70
 
 def get_mentions():
     c = 25
+    twiidiala.delete(ALL)#teeme tahvli puhtaks
     statuses = twitter.statuses.mentions_timeline()
-    for a in range(0,5):
+    for a in range(0,12):
         x = statuses[a]
         tweet = x['text']
         user = (x['user'])['screen_name']
         name = (x['user'])['name']
-        b = 320
-        twiit(name+'   '+"@"+user+"\n"+tweet+'\n'*2, b, c)
+        parem_twiit(name, user, tweet, c)
         c += 70
                 
         
@@ -105,11 +118,21 @@ raam.geometry("630x400")
 raam.configure(background = color1)
 raam.option_add("*Font", ("Segoe UI", 10))#font
 paksFont = ("Times", 20, "bold")
-raam.resizable(width=FALSE, height=FALSE)
+#raam.resizable(width=FALSE, height=FALSE)
+
+###loome scrollbari
+twiidiala = Canvas(raam, width = 380, height = 370)
+scrollbar = ttk.Scrollbar(twiidiala)
+twiidiala.configure(yscrollcommand = scrollbar.set, scrollregion = (0,0,320,850), highlightthickness = 0)
+twiidiala.configure(bg = color1)
+twiidiala.place(x = 240, y = 24)
+scrollbar.place(x = 363, y = 0, height = 370)
+scrollbar.config(command=twiidiala.yview)
+
 
 CONSUMER_KEY = "OyremhLVargLoqBAG2PZwQ" #voldemari consumer key
 CONSUMER_SECRET = "25GrCT1ItNRnHmMQc4QRD1qUpm8jvY1HTzsaYHqLCBE" #voldemari consumer secret
-kasutajanimi = "erx0r" #kasutajanimi
+kasutajanimi = "MadisKarli" #kasutajanimi
 if not isfile(kasutajanimi+'.txt'): #kui vastavat faili veel pole siis loob selle
     oauth_dance('voldemar', 'OyremhLVargLoqBAG2PZwQ', '25GrCT1ItNRnHmMQc4QRD1qUpm8jvY1HTzsaYHqLCBE',kasutajanimi+'.txt')
 f=open(kasutajanimi+'.txt') #avab faili ja võtab sealt oauth info
@@ -135,6 +158,7 @@ nupp4.grid(column=6, row=1)
 nupp5 = ttk.Button(raam, text="Säutsu", command = säutsumine)
 nupp5.grid(column=1, row=6, pady=5, sticky = (E))
 
+
 #säutsu sisestamine, uuri ttk.Text
 twiidikast = Text(raam, width=30, height=1, wrap = 'word')
 twiidikast.grid(column=1, row = 5)
@@ -147,19 +171,11 @@ Skyglow = ttk.Label(raam, text = 'Skyglow ©®', anchor = 'e')
 Skyglow.configure(background = color1, foreground = "white")
 Skyglow.grid(column = 1, row = 1)
 
-#loob teksti
+#tervitab kasutajat
 username = ttk.Label(raam, text = 'Tere, '+kasutajanimi+ '!')
 username.configure(background = color1, foreground = "white")
 username.place(x = 0, y = 220)
 
-###loome scrollbari
-##twiidiala = Canvas(raam, width = 320, height = 400)
-##scrollbar = ttk.Scrollbar(twiidiala)
-##frame = Frame(twiidiala)
-##twiidiala.configure(yscrollcommand = scrollbar.set, scrollregion = (0,0,310,390))
-##twiidiala.place(x = 300, y = 24)
-##scrollbar.place(x = 310, y = 0, height = 400)
-##scrollbar.config(command=twiidiala.yview)
 
 
 ##space = ttk.Label(raam, text = '')
